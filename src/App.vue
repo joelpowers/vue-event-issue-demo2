8e4deb6104f8d9fe1aa0a4d7fd8f58df143c9540
Joel Powers <joelpowers@gmail.com>
2017-03-27 20:08:51 -0500

    initial commit - built from vue cli

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <emit></emit>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+import Emit from './components/Emit';
+
+export default {
+  name: 'app',
+  components: {
+    Hello, Emit,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Emit.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="emit">
+    <h1>{{emsg}}</h1>
+    <div>
+      <label>Camera Z position</label>
+      <input type="range" v-model="camZdeg" min="0" max="359">
+      <span>{{camZdeg}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  /* eslint-disable no-console */
+  import Vue from 'vue';
+  import VueEvents from 'vue-events';
+
+  Vue.use(VueEvents);
+
+  export default {
+    name: 'emit',
+    data() {
+      return {
+        emsg: 'emit',
+        camZdeg: 0,
+      };
+    },
+    watch: {
+      camZdeg(nDeg) {
+        console.log('emit');
+        this.$events.emit('cameraDeltaX', nDeg);
+      },
+    },
+  };
+</script>
